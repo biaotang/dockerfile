68c454a7e2116ddf4cc6dc822a234ba522dfc8e7
Bill <biaotangwork@gmail.com>
2019-09-29 19:08:19 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,6 +10,13 @@ USER=${USER:-root}
 PASSWORD=${PASSWORD:-alpine@}
 ROOT_PASSWORD=${ROOT_PASSWORD}
 
+LANG="${LANG:-zh_CN}.UTF-8"
+
+echo "LANG=$LANG" >> /etc/profile
+echo "LANGUAGE=$LANG" >> /etc/profile
+echo "LC_ALL=$LANG" >> /etc/profile
+source /etc/profile
+
 if [[ "$USER" != "root" ]]; then
    if ! id -u $USER > /dev/null 2>&1; then
       adduser -u 1000 $USER -G root -D
